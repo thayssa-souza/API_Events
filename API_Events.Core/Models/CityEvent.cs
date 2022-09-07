b530de07a60abe09332095a91df1aa2597419571
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-09-07 20:03:17 -0300

    Created models folder on Core

--- API_Events.Core/Models/CityEvent.cs
+++ API_Events.Core/Models/CityEvent.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace API_Events.Core.Models
 {
-    internal class CityEvent
+    public class CityEvent
     {
+        public long IdEvent { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateHourEvent { get; set; }
+        public string Local { get; set; }
+        public string Address { get; set; }
+        public decimal Price { get; set; }
     }
 }
---
+++ API_Events.Core/Models/EventReservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Events.Core.Models
+{
+    public class EventReservation
+    {
+        public long IdReservation { get; set; }
+        public long IdEvent { get; set; }
+        public string PersonName { get; set; }
+        public long Quantity { get; set; }
+    }
+}
